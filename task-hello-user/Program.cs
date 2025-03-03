5a0db0d755be9abd135e5db21502c1d8b2f5d036
Elvira B <be17022025@gmail.com>
2025-03-03 23:10:21 +0100

    Task hello user
    * Make basic files for task
    
    * Create code for task hello-user

---
+++ task-hello-user/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+
+partial class Program
+{
+    static void Main()
+    {
+        Console.Write("Write your name here: ");
+        string? myName = Console.ReadLine();
+        Console.WriteLine();
+
+        Console.Write("What is your age? ");
+        int myAge = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        Person person = new Person(myName, myAge);
+        Console.WriteLine($"Hello {person.Name}, you are {person.Age} years old.");
+    }
+}
+
+class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string? newName, int newAge) // Constructor
+    {
+        this.Age = newAge;
+        this.Name = newName;
+    }
+}
+

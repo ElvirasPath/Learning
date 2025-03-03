375b04d329fd982bcc53ad4e05d0c512eb1c3573
Elvira B <be17022025@gmail.com>
2025-03-03 20:51:16 +0100

    Learn how to write HelloWorld using C#
    * Learn how to write HelloWorld using C#
    
    * Add gitignore
    
    * Update gitignore

---
+++ C# devkit/HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine("Hello, World!");
+
+public static class Robot
+{
+    public static string sayHi()
+    {
+        string hi = "hi";
+        Console.WriteLine(hi);
+        return hi;
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+
+class Programm
+{
+    static void Main()
+    {
+        Person john = new Person { Name = "John", Age = 30 };
+        Person beth = new Person { Name = "Beth", Age = 23 };
+        Person alex = new Person { Name = "Alex", Age = 40 };
+        Console.WriteLine(john.Name);
+    }
+}

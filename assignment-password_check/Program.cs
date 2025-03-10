e50a1bbb67058a0f9c20ed48156902f1d6424c92
Elvira B <be17022025@gmail.com>
2025-03-10 15:21:23 +0100

    Initial commit + first attempt on making password checker (#7)
    * Innitial commit + first attempt on making password checker
    
    * Minor fixes for clearer code
    
    * Minor lines fixes for clearer code
    
    * Change for loop to foreach loop + try to implement do/whileloop
    
    * Do/While Loop fix
    
    * Minor fixes for clearer code
    
    ---------
    
    Co-authored-by: Elvira Bihdan <bigdan.elvira@gmail.com>

---
+++ assignment-password_check/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string? specialChars = "!?.@:";
+        bool hasUpper = false, hasLower = false, hasSpecial = false; //Initially, we don’t know if the password has an uppercase letter, lowercase letter, or special character.
+
+        do
+        {
+            Console.Write("Create your password: ");
+            string? newPassword = Console.ReadLine();
+
+            if (newPassword?.Length < 5)
+            {
+                Console.WriteLine("❌Weak password, please enter at least 5 characters");
+                return;
+            }
+            foreach (char character in newPassword)
+            {
+                if (specialChars.Contains(character)) hasSpecial = true;
+                if (char.IsLower(character)) hasLower = true;
+                if (char.IsUpper(character)) hasUpper = true;
+            }
+        
+            //Check if all conditions are met 
+            if (hasSpecial && hasUpper && hasLower)
+            {
+                Console.WriteLine("✅ Strong password");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("❌Password must contain at least one uppercase and lower letter, together with at lest one special character");
+            }
+        } while (!(hasSpecial && hasUpper && hasLower));
+    } 
+}

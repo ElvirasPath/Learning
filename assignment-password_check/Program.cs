d3090d368ea8b045c7c43e338b7098507367539e
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-10 14:35:58 +0100

    Change for loop to foreach loop + try to implement do/whileloop

--- assignment-password_check/Program.cs
+++ assignment-password_check/Program.cs
@@ -5,32 +5,41 @@
         string? specialChars = "!?.@:";
         bool hasUpper = false, hasLower = false, hasSpecial = false; //Initially, we don’t know if the password has an uppercase letter, lowercase letter, or special character.
 
-        Console.Write("Create your password: ");
-        string? newPassword = Console.ReadLine();
-
-        if (newPassword?.Length < 5)
+        do
         {
-            Console.WriteLine("❌Weak password, please enter at least 5 characters");
-            return;
-        }
+            Console.Write("Create your password: ");
+            string? newPassword = Console.ReadLine();
 
-        for (int i = 0; i < newPassword?.Length; i++)
-        {
-            char character = newPassword[i]; // Get each character at index i 
+            if (newPassword?.Length < 5)
+            {
+                Console.WriteLine("❌Weak password, please enter at least 5 characters");
+                return;
+            }
+            foreach (char character in newPassword)
+            {
+                if (specialChars.Contains(character)) hasSpecial = true;
+                if (char.IsLower(character)) hasLower = true;
+                if (char.IsUpper(character)) hasUpper = true;
+            }
+            // for (int i = 0; i < newPassword?.Length; i++)
+            // {
+            //     char character = newPassword[i]; // Get each character at index i 
 
-            if (specialChars.Contains(newPassword[i])) hasSpecial = true;
-            if (char.IsLower(character)) hasLower = true;
-            if (char.IsUpper(character)) hasUpper = true;
-        }
-        //Check if all conditions are met 
-        if (hasSpecial && hasUpper && hasLower)
-        {
-            Console.WriteLine("✅ Strong password");
-        }
-        else
-        {
-            Console.WriteLine("❌Password must contain at least one uppercase and lower letter, together with at lest one special character");
-        }
-    }
+            //     if (specialChars.Contains(newPassword[i])) hasSpecial = true;
+            //     if (char.IsLower(character)) hasLower = true;
+            //     if (char.IsUpper(character)) hasUpper = true;
+            // }
 
+            //Check if all conditions are met 
+            if (hasSpecial && hasUpper && hasLower)
+            {
+                Console.WriteLine("✅ Strong password");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("❌Password must contain at least one uppercase and lower letter, together with at lest one special character");
+            }
+        } while (hasSpecial && hasUpper && hasLower);
+    } 
 }

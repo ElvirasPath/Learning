c14b6dce876b155e442ad1b3c360deba03bdae54
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-17 12:57:31 +0100

    Compele code for prime number checker.Attempt 1

--- c#-basic-training/Program.cs
+++ c#-basic-training/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace primenumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        do
+        {
+            Console.WriteLine("Enter a number: ");
+            string? userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out int result))
+            {
+                for (int i = 2; i <= result; i++)
+                {
+                    
+                    int remainder = result % 2;
+                    if (remainder == 0)
+                    {
+                        break;
+                    }
+                    if (remainder != 0)
+                    {
+                        Console.WriteLine("Prime number");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a prime number");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("❌ Enter a number, please.");
+                continue;
+            }
+
+            Console.WriteLine();
+            Console.Write("Do you want to exit?(yes / no): ");
+            string? exit = Console.ReadLine();
+
+            if (exit == "yes")
+            {
+                break;
+            }
+
+        } while (true);
+    }
+}

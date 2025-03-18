ec228f9af2eed65135893e515c54b15fd47419a3
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-18 12:26:06 +0100

    Add do/while loop

--- c#-basic-reverse-task/Program.cs
+++ c#-basic-reverse-task/Program.cs
@@ -3,17 +3,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter your word: ");
-        string? userInput = Console.ReadLine();
-        string? reversed = string.Empty;
-
-        for (int i = userInput.Length - 1; i >= 0; i--)
+        do
         {
-            reversed = reversed + userInput[i];
-        }
+            Console.Write("Enter your word: ");
+            string? userInput = Console.ReadLine();
+            string? reversed = string.Empty;
 
-        Console.WriteLine();
-        Console.WriteLine($"Your reversed result: {reversed}");
+    
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                reversed = reversed + userInput[i];
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Your reversed result: {reversed}");
+
+            Console.WriteLine();
+            Console.Write("Write 'end' if you want to end your session: ");
+            string? exit = Console.ReadLine();
+            if (exit == "end")
+            {
+                break;
+            }
+        } while (true);
     }
 }
  

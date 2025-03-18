cb868309e424f6d4c5b598cddf94174524e7c45d
Elvira B <be17022025@gmail.com>
2025-03-18 09:44:11 +0100

    Prime number checker - C# basic training  (#13)
    * Initial commit
    
    * Initial commit 2
    
    * Complete multiplication table task and README.md file
    
    * Initial commit
    
    * Complete README.md file
    
    * Compele code for prime number checker.Attempt 1
    
    * Fix the code for the task
    
    * Add more efficient code for task
    
    * Line fixes for clearer code
    
    ---------
    
    Co-authored-by: Elvira Bihdan <bigdan.elvira@gmail.com>

---
+++ c#-basic-training/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace primenumber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write("Enter a number: ");
+            string? userInput = Console.ReadLine();
+            if (long.TryParse(userInput, out long result))
+            {
+                bool isPrime = IsPrime(result);
+                if (isPrime == true)
+                {
+                    Console.WriteLine("Prime number");
+                }
+                else
+                {
+                    Console.WriteLine("Not a prime number");
+                }
+            }
+            else
+            {
+                Console.WriteLine("❌ Enter a number, please.");
+                continue;
+            }
+
+            Console.WriteLine();
+            Console.Write("Do you want to exit?(yes / no): ");
+
+            string? exit = Console.ReadLine();
+            if (exit == "yes")
+            {
+                break;
+            }
+        } while (true);
+    }
+
+    static bool IsPrime(long number)
+    {
+        if (number == 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

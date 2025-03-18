8bb0d6a8040199c26c095784d78a6f6e945af646
Elvira B <be17022025@gmail.com>
2025-03-18 09:43:57 +0100

    Multiplication task (#12)
    * Initial commit
    
    * Initial commit 2
    
    * Complete multiplication table task and README.md file
    
    ---------
    
    Co-authored-by: Elvira Bihdan <bigdan.elvira@gmail.com>

---
+++ multiplication-task/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //string? userInput = Console.ReadLine();
+
+        try
+        {
+            string? userInput = args[0];
+            bool hasParsed = int.TryParse(userInput, out int result); 
+            if (hasParsed)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    Console.WriteLine($"{result} * {i} = {result * i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong operation");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Sorry, something went wrong: {ex.Message}");
+        }
+    }
+}

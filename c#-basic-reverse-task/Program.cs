0f62fd256fb3dc9d7a20ebb0a05284b4cbbaad58
Elvira B <be17022025@gmail.com>
2025-03-20 14:58:35 +0100

    C# reverse a word task (#14)
    * Initial commit
    
    * Complete README.md file
    
    * Complete code for the task.Attempt 1
    
    * Add do/while loop
    
    * Minor line fixes
    
    * Add a new method/function
    
    * Function/Method fixes
    
    * Lines fixes for clearer code
    
    * Small fixes for clearer code
    
    * Make small code modifications
    
    ---------
    
    Co-authored-by: Elvira Bihdan <bigdan.elvira@gmail.com>

---
+++ c#-basic-reverse-task/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace training;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        do
+        {
+            Console.Write("Enter a word: ");
+            string? userInput = Console.ReadLine();
+            string reversed = Reverse(userInput);
+            Console.WriteLine();
+            Console.WriteLine($"Reversed result: {reversed}");
+
+            Console.WriteLine();
+            Console.Write("Write 'end' if you want to end your session: ");
+
+            string? exit = Console.ReadLine();
+            if (exit == "end")
+            {
+                break;
+            }
+        } while (true);
+    }
+
+    static string Reverse(string? userInput)
+    {
+        if (userInput is null)
+        {
+            return string.Empty;
+        }
+
+        string reversed = string.Empty;
+        for (int i = userInput.Length - 1; i >= 0; i--)
+        {
+            reversed += userInput[i];
+        }
+
+        return reversed;
+    }
+}

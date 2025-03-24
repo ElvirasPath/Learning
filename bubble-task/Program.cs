08483ccbca3206fc53a4378fd752b2c9775d181b
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-24 22:17:47 +0100

    Complete task

--- bubble-task/Program.cs
+++ bubble-task/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter your numbers: ");
+        Console.Write("Enter your numbers separated by spaces: ");
         string? myNumbersString = Console.ReadLine();
 
         if (myNumbersString is null)
@@ -15,18 +15,26 @@ class Program
             return;
         }
 
-        string[] arrayOfStrings = myNumbersString.Split(' ');
+        string[] arrayOfStrings = myNumbersString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        int[] numbers = new int[arrayOfStrings.Length];
 
-        Console.WriteLine("Before: ");
-        foreach (int i in myNumbers)
+        for (int i = 0; i < arrayOfStrings.Length; i++)
         {
-            Console.Write($"{i} ");
+            if (int.TryParse(arrayOfStrings[i], out int number))
+            {
+                numbers[i] = number;
+            }
+            else
+            {
+                Console.WriteLine("Wrong operation");
+                return;
+            }
         }
 
-        myNumbers = Sort(myNumbers);
+        numbers = Sort(numbers);
         Console.WriteLine();
-        Console.WriteLine("After: ");
-        foreach (int i in myNumbers)
+        Console.Write("After: ");
+        foreach (int i in numbers)
         {
             Console.Write($"{i} ");
         }

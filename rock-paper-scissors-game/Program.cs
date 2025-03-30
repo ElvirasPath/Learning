deed3edad851d5570efd0e9f84d0fdf28455c957
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-30 23:25:31 +0200

    Temporary change the logic

--- rock-paper-scissors-game/Program.cs
+++ rock-paper-scissors-game/Program.cs
@@ -4,29 +4,27 @@ namespace rpsgame;
 
 class Program
 {
-    
+
     static void Main(string[] args)
     {
         int userScore = 0;
         int computerScore = 0;
         Console.WriteLine("Welcome to rock, paper or scissiors game!");
 
-        while (userScore != 3 && computerScore != 3)
-        {
+        //while (userScore != 3 || computerScore != 3)
+       // {
             Console.WriteLine($"User score: {userScore}. Computer score: {computerScore}.");
             Console.WriteLine("Choose between rock, paper or scissiors: ");
             string? userInput = Console.ReadLine();
-        }
+
+            
+        //}
 
         Random random = new Random();
 
         string[] commands = ["rock", "paper", "scissors"];
         int computersChoice = random.Next(commands.Length);
         Console.WriteLine($"Computer's choice: {commands[computersChoice]}");
-    }
-
-    static string Result(string result)
-    {
 
         if (userInput == commands[0] && commands[computersChoice] == commands[1]) return ("You lost!");
         else if (userInput == commands[1] && commands[computersChoice] == commands[0]) return ("You win!");
@@ -35,5 +33,6 @@ class Program
         else if (userInput == commands[0] && commands[computersChoice] == commands[2]) return ("You lost!");
         else if (userInput == commands[2] && commands[computersChoice] == commands[0]) return ("You win!");
         else Console.WriteLine("It's a tie, nobody wins!");
+    
     }
 }    

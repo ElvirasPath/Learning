9801bd93285bd94c9f9affc0bd03c5c9dfcb14f5
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-30 23:08:03 +0200

    Complete code for basic version of the game

--- rock-paper-scissors-game/Program.cs
+++ rock-paper-scissors-game/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+namespace rpsgame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        Console.WriteLine("Choose between rock, paper or scissiors: ");
+        string? userInput = Console.ReadLine();
+
+        string[] commands = ["rock", "paper", "scissors"];
+        int computersChoice = random.Next(commands.Length);
+        Console.WriteLine($"Computer's choice: {commands[computersChoice]}");
+
+
+        if (userInput == commands[0] && commands[computersChoice] == commands[1]) Console.WriteLine("You lost!");
+        else if (userInput == commands[1] && commands[computersChoice] == commands[0]) Console.WriteLine("You win!");
+        else if (userInput == commands[1] && commands[computersChoice] == commands[2]) Console.WriteLine("You lost!");
+        else if (userInput == commands[2] && commands[computersChoice] == commands[1]) Console.WriteLine("You win!");
+        else if (userInput == commands[0] && commands[computersChoice] == commands[2]) Console.WriteLine("You lost!");
+        else if (userInput == commands[2] && commands[computersChoice] == commands[0]) Console.WriteLine("You win!");
+        else Console.WriteLine("It's a draw, nobody wins!");
+    }
+}    

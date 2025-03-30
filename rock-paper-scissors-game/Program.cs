a563e2ff3811c3b73b27ea71d45be9d4d4f1e5e8
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-03-30 23:20:54 +0200

    Work on better features for the game

--- rock-paper-scissors-game/Program.cs
+++ rock-paper-scissors-game/Program.cs
@@ -4,24 +4,36 @@ namespace rpsgame;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Random random = new Random();
+        int userScore = 0;
+        int computerScore = 0;
+        Console.WriteLine("Welcome to rock, paper or scissiors game!");
 
-        Console.WriteLine("Choose between rock, paper or scissiors: ");
-        string? userInput = Console.ReadLine();
+        while (userScore != 3 && computerScore != 3)
+        {
+            Console.WriteLine($"User score: {userScore}. Computer score: {computerScore}.");
+            Console.WriteLine("Choose between rock, paper or scissiors: ");
+            string? userInput = Console.ReadLine();
+        }
+
+        Random random = new Random();
 
         string[] commands = ["rock", "paper", "scissors"];
         int computersChoice = random.Next(commands.Length);
         Console.WriteLine($"Computer's choice: {commands[computersChoice]}");
+    }
 
+    static string Result(string result)
+    {
 
-        if (userInput == commands[0] && commands[computersChoice] == commands[1]) Console.WriteLine("You lost!");
-        else if (userInput == commands[1] && commands[computersChoice] == commands[0]) Console.WriteLine("You win!");
-        else if (userInput == commands[1] && commands[computersChoice] == commands[2]) Console.WriteLine("You lost!");
-        else if (userInput == commands[2] && commands[computersChoice] == commands[1]) Console.WriteLine("You win!");
-        else if (userInput == commands[0] && commands[computersChoice] == commands[2]) Console.WriteLine("You lost!");
-        else if (userInput == commands[2] && commands[computersChoice] == commands[0]) Console.WriteLine("You win!");
-        else Console.WriteLine("It's a draw, nobody wins!");
+        if (userInput == commands[0] && commands[computersChoice] == commands[1]) return ("You lost!");
+        else if (userInput == commands[1] && commands[computersChoice] == commands[0]) return ("You win!");
+        else if (userInput == commands[1] && commands[computersChoice] == commands[2]) return ("You lost!");
+        else if (userInput == commands[2] && commands[computersChoice] == commands[1]) return ("You win!");
+        else if (userInput == commands[0] && commands[computersChoice] == commands[2]) return ("You lost!");
+        else if (userInput == commands[2] && commands[computersChoice] == commands[0]) return ("You win!");
+        else Console.WriteLine("It's a tie, nobody wins!");
     }
 }    

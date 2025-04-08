b71ac5339ba2f3328c03c003cd50300c82d138dc
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-04-08 14:43:22 +0200

    Complete task

--- LinQ-operations/Program.cs
+++ LinQ-operations/Program.cs
@@ -1,7 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
+    //Language Integrated Query
+    //Operate on IEnumerable<T>
+    //LINQ helps with mapping, filtering and reducing
+    //map: transform each element in a collection 
+    // LINQ	A way to query/filter/sort data in C#
+    //Lambda	A short function written like x => x > 0
+    //Predicate	A function that returns true or false (often used in filtering)
     static void Main(string[] args)
     {
         Student student = new Student();
@@ -14,7 +23,78 @@ class Program
             new Student { Name = "Evan", Age = 18, Grade = 60, Class = "C" },
             new Student { Name = "Fiona", Age = 17, Grade = 77, Class = "C" }
         };
-    }
+
+        // LINQ with lambda predicate to get grades > 80
+        // var topStudents = from s in students
+        //                   where s.Grade >= 80
+        //                   select s; 
+        //.Where(...) → LINQ method; n => n > 20 → lambda expression; n > 20 → predicate condition
+
+        var topStudents = students.Where(s => s.Grade >= 80);
+        Console.WriteLine("Top students: ");
+        foreach (var student1 in topStudents)//
+        {
+            Console.WriteLine($"{student1.Name} - Grade: {student1.Grade}");
+        }
+        Console.WriteLine();
+        // bool HasGoodGrade(Student s)
+        // {
+        //     return s.Grade >= 80;
+        // }
+        // var greatGradeStudents = students.Where(HasGoodGrade);
+        //foreach (var student in topStudents) Console.WriteLine($"{student1.Name} - Grade: {student1.Grade}");
+
+        var sortedStudents = students.OrderBy(s => s.Name);
+        Console.Write("Ordered students: ");
+        foreach (var student1 in sortedStudents)
+        {
+            Console.Write($"{student1.Name}; ");
+        }
+        Console.WriteLine();
+
+        var gropedByClass = students.GroupBy(s => s.Class); // groups students based on the Class value
+        foreach (var group in gropedByClass)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Class {group.Key}"); // gives you the class name (A,B,C)
+            foreach (var student1 in group)
+            {
+                Console.WriteLine($" - {student1.Name}");
+            }
+        }
+        Console.WriteLine();
+        double averageGrade = students.Average(s => s.Grade);
+        Console.WriteLine($"Average Grade: {averageGrade}");
+        Console.WriteLine();
+
+        // Predicate<Student> hasPassed = s => s.Grade >= 50;
+        // double averagePassingGrade = students
+        //         .Where(s => hasPassed(s))
+        //         .Average(s => s.Grade);
+        // Console.WriteLine($"Average passing grade {averagePassingGrade}");
+
+        //Filter -> Sort Desc -> Select Names)
+        Console.WriteLine("High Scorers:");
+        var highScorers = students
+                .Where(s => s.Grade > 90)
+                .OrderByDescending(s => s.Grade)
+                .Select(s => s.Name);
+
+        foreach (var name in highScorers)
+        {
+            Console.Write($"{name}; ");
+        }
+
+        // Predicate<Student> isHighScorer = s => s.Grade > 75;
+        // var highScorerNames = students
+        //         .Where(s => isHighScorer(s)) using predicate
+        //         .OrderByDescending(s => s.Grade)
+        //         .Select(s => s.Name);
+        // foreach (var name on highScorerNames)
+        // {
+        //     Console.Write($"{name}; ");
+        // }
+    }           
 }
 
 public class Student
@@ -27,11 +107,11 @@ public class Student
 
     public string? Class { get; set; }
 
-    // public Student(string? name, int age, int grade, string? class)
+    // public Student(string? name, int age, int grade, string? @class)
     // {
     //     Name = name;
     //     Age = age;
     //     Grade = grade;
-
+    //     Class = @class;
     // }
 }

63d5e5901fb89876aa573d94a8d266aff9003160
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-05-05 10:29:02 +0200

    init commit

---
+++ csv-learning/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace csv_learning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"/Users/pro/Projects/Learning/csv-learning/currency.csv";
+
+        if (File.Exists(filePath))
+        {
+            var lines = File.Re
+        }
+    }
+}
+

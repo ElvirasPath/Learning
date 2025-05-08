e403c56e44470a2e57a4de51aff82b59d5e81c43
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-05-08 14:30:28 +0200

    Add class, fields and method for CardPicker.cs

--- card-picker/CardPicker.cs
+++ card-picker/CardPicker.cs
@@ -1,2 +1,26 @@
 namespace cardpicker;
 
+class CardPicker
+{
+    string[] cards = {
+        "10 of Diamonds",
+        "6 of Clubs",
+        "7 of Spades",
+        "Ace of Diamonds",
+        "Ace of Hearts"
+    };
+
+    public string[] PickSomeCards(int numberOfCards)
+    {
+        Random rand = new Random();
+        string[] pickedCards = new string[numberOfCards];
+
+        for (int i = 0; i < numberOfCards; i++)
+        {
+            int index = rand.Next(cards.Length);
+            pickedCards[i] = cards[index];
+        }
+
+        return pickedCards;
+    }
+}
--- card-picker/Program.cs
+++ card-picker/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        string? input = Console.ReadLine();
 
+        if (int.TryParse(input, out int numberOfCards))
+        {
+            Console.WriteLine(numberOfCards);
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
     }
 }

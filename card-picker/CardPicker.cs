e2b4257af7f4869a2a9edaf8863788575e06d8c6
Elvira Bihdan <bigdan.elvira@gmail.com>
2025-05-09 10:55:44 +0200

    complete task

--- card-picker/CardPicker.cs
+++ card-picker/CardPicker.cs
@@ -1,26 +1,46 @@
-namespace cardpicker;
+namespace card_picker;
 
-class CardPicker
-{
-    string[] cards = {
-        "10 of Diamonds",
-        "6 of Clubs",
-        "7 of Spades",
-        "Ace of Diamonds",
-        "Ace of Hearts"
-    };
+public class CardPicker
+{ 
+    // string[]? cards = [
+    //     "10 of Diamonds",
+    //     "6 of Clubs",
+    //     "7 of Spades",
+    //     "Ace of Diamonds",
+    //     "Ace of Hearts"
+    // ];
 
-    public string[] PickSomeCards(int numberOfCards)
+    public static string[] PickSomeCards(int numberOfCards)
     {
-        Random rand = new Random();
         string[] pickedCards = new string[numberOfCards];
 
         for (int i = 0; i < numberOfCards; i++)
         {
-            int index = rand.Next(cards.Length);
-            pickedCards[i] = cards[index];
+            // int index = random.Next(cards!.Length);
+            pickedCards[i] = $"{RandomValue()} of {RansomSuit()};";
         }
 
         return pickedCards;
     }
+
+    private static string RansomSuit()
+    {
+        // Random random = new Random();
+        int value = Random.Shared.Next(1, 5); // pick a random number from 1 to 4
+        if (value == 1) return "Spades"; 
+        if (value == 2) return "Hearts";
+        if (value == 3) return "Clubs";
+        return "Diamonds"; // otherwise return the string Diamonds
+    }
+
+    private static string RandomValue()
+    {
+        int value = Random.Shared.Next(1, 14);
+        if (value == 1) return "Ace"; 
+        if (value == 11) return "Jack";
+        if (value == 12) return "Queen";
+        if (value == 13) return "King";
+        return value.ToString();
+    }
 }
+
--- card-picker/Program.cs
+++ card-picker/Program.cs
@@ -1,19 +1,29 @@
-﻿namespace cardpicker;
+﻿using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
 
-class Program
+namespace card_picker;
+
+
+internal class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Please enter a number of cards to pick: ");
         string? input = Console.ReadLine();
 
         if (int.TryParse(input, out int numberOfCards))
         {
+            string[] cards = CardPicker.PickSomeCards(numberOfCards);
             Console.WriteLine(numberOfCards);
-            return;
+            foreach (string card in cards)
+            {
+                Console.WriteLine(card);
+            }
         }
         else
         {
-            Console.WriteLine("Invalid input");
+            Console.WriteLine("Please enter a number");
         }
     }
 }
+
